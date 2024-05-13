cfcaaff7034f39014a0eb14de88601d52a99f36b
Aasan-Kumar <159622950+Aasan-Kumar@users.noreply.github.com>
2024-05-13 00:15:19 +0000

    variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,12 @@
+const accountId = 112233
+let accountEmail = "abc@google.com"
+var accountPassword = "1234"
+accountCity = "Hyderabad"
+
+///accountId = 123  => Constant value changes not allow
+
+//console.log(
+  //  accountId
+//);
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
