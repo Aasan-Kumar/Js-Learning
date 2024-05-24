4e1909329ecf25a7e232530142307ccf8fd55b96
Aasan-Kumar <159622950+Aasan-Kumar@users.noreply.github.com>
2024-05-24 11:51:03 +0000

    datatype summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,35 @@
+// Primitive Datatype
+
+// 7 Types: String, Number, null, Boolean, Undefine, Symbol, BigInt
+
+// Reference (Non Primitive)
+
+// Array , Objects ,Functions
+
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false 
+const outsideTem = null
+
+let userEmail; //it is automatically Undefine assign
+//console.log( userEmail);
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+//console.log(id === anotherId);
+
+const bigNumber = 1287364387623475252432432432n
+
+const cities = ["Hyderabad","Karachi","Lahore"]
+
+let myObj ={
+    name:'Aasan',
+    age:'30'
+}
+
+const myfunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof id);
