6c0f405907316a942687bf74d7e3bf7222a7d5b0
Aasan-Kumar <159622950+Aasan-Kumar@users.noreply.github.com>
2024-05-24 12:32:59 +0000

    heap & stack memory

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -19,10 +19,11 @@ const anotherId = Symbol('123')
 
 //console.log(id === anotherId);
 
-const bigNumber = 1287364387623475252432432432n
+const bigNumber = 1287364387623475252432432432n // BigInt
 
-const cities = ["Hyderabad","Karachi","Lahore"]
+const cities = ["Hyderabad","Karachi","Lahore"] // Array
 
+//Object
 let myObj ={
     name:'Aasan',
     age:'30'
@@ -32,4 +33,31 @@ const myfunction = function(){
     console.log("Hello World");
 }
 
-console.log(typeof id);
+//console.log(typeof id);
+
+//**********************Memory*************************** */
+
+//Stack(Primitive) .... Heap(Non Primitive)
+
+let countryName = "Pakistan"
+
+let anotherCountry = countryName;
+
+anotherCountry = "Dubai"
+//Stact memory
+// console.log(countryName);
+// console.log(anotherCountry);
+
+
+//Heap memory
+let userOne = {
+    email:"aasan@abc.com",
+    name: "aasan"
+}
+
+let userTwo = userOne 
+
+userTwo.email = "hitesh@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
