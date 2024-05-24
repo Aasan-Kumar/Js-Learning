d241465dbc71ad48713234c070c503607505df09
Aasan-Kumar <159622950+Aasan-Kumar@users.noreply.github.com>
2024-05-24 09:42:46 +0000

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -16,7 +16,7 @@ let isLoggedId = ""
 
 let booleanIsLoggedIn = Boolean(isLoggedId);
 
-console.log(booleanIsLoggedIn)
+//console.log(booleanIsLoggedIn)
 
 // 1 => true; 0 => false
 // "" => false
@@ -26,4 +26,51 @@ let someNumber = 33
 
 let stringNumber = String(someNumber)
 
-console.log(typeof stringNumber)
+//console.log(typeof stringNumber)
+
+// *******************Operations*******************
+
+let value = 3
+
+let negValue = -value
+
+//console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**3);
+//console.log(2/3);
+//console.log(2%3);
+
+let = str1 = "Hello"
+let = str2 = " Aasan"
+let = str3 = str1 + str2
+
+//console.log(str3);
+
+//console.log("1" + 2);
+
+//console.log(1 + "2");
+
+//console.log("1" + "2");
+
+//console.log("1"+ 2 + 2); String first come then operation not works
+
+//console.log(1 + 2 + "2"); Number first come then operation works
+
+//console.log(true);
+
+//console.log(+true);
+
+//console.log(+"");
+
+let gameCounter = 100
+
+//++gameCounter;
+
+//gameCounter++;
+
+//console.log(gameCounter);
+
+
